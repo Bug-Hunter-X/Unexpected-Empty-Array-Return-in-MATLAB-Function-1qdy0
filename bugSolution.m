beefe9f5f5d540212c3fd2dded84fd14812c254b
BugHunter-X <askzenai@gmail.com>
2024-12-21 03:24:05 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,9 @@
+function result = myFunction(input)
+  % Some code here...
+  if someCondition
+    result = someValue;
+  else
+    result = 0; % Or NaN, depending on the context
+  end
+  % More code here...
+end
